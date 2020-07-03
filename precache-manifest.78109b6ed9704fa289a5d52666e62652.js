dd47e62e9b13c9b72482474e410e74d3a541776e
easyvipin <vipinchandra555@gmail.com>
2020-07-03 17:37:44 +0530

    Updates

---
+++ precache-manifest.78109b6ed9704fa289a5d52666e62652.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a3d10152f4ec77ea034594b432a926",
+    "url": "/Easyvipin/smile-app/index.html"
+  },
+  {
+    "revision": "0e8592ffc119006ecc8f",
+    "url": "/Easyvipin/smile-app/static/css/main.d82a7666.chunk.css"
+  },
+  {
+    "revision": "9adeaf2c947236a20856",
+    "url": "/Easyvipin/smile-app/static/js/2.1e172582.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Easyvipin/smile-app/static/js/2.1e172582.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e8592ffc119006ecc8f",
+    "url": "/Easyvipin/smile-app/static/js/main.a01da6b9.chunk.js"
+  },
+  {
+    "revision": "ba40158a0895522461b4",
+    "url": "/Easyvipin/smile-app/static/js/runtime-main.eb97cbd3.js"
+  }
+]);
